4cd5e706d4ebb2e241d2ca2ee065f2528c404ecc
spcjw <prsjb77@gmail.com>
2020-12-29 16:53:58 +0900

    check nickname

---
+++ src/api/user/checkNickname/checkNickname.js
@@ -0,0 +1,22 @@
+import { prisma } from "../../../../prisma/prismaClient";
+
+export default {
+  Mutation: {
+    checkNickname: async (_, args) => {
+      const { nickname } = args;
+      const user = await prisma.user.findUnique({
+        where: {
+          nickname,
+        },
+      });
+
+      if (user) {
+        // 사용할 수 없음
+        return false;
+      } else {
+        // 사용할 수 있음
+        return true;
+      }
+    },
+  },
+};
--- src/api/user/updateUser/updateUser.js
+++ src/api/user/updateUser/updateUser.js
@@ -6,6 +6,16 @@ export default {
       isAuthenticated(request);
       const { userId, avatar, intro, nickname } = args;
 
+      const check = prisma.user.findUnique({
+        where: {
+          nickname,
+        },
+      });
+
+      if (check) {
+        throw Error("이미 존재하는 닉네임입니다.");
+      }
+
       return prisma.user.update({
         where: {
           uuid: userId,
